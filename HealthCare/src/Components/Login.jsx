a9ccfc7c644ae172bf0c24a6b32a270ff6c03fa6
AjinkyaDsarkase <ajinkya.darkase12@gmail.com>
2025-06-27 13:16:51 +0530

    Project creation

---
+++ HealthCare/src/Components/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import LogoHead from './LogoHead'
+import heaxagon from '../assets/hexagon.jpg'
+import cubes from '../assets/cubes.jpg'
+import '../styles/Login.css'
+
+function Login() {
+  return (
+    <>
+        <div className = 'login-form'>
+            <img src="https://preclinic.dreamstechnologies.com/html/template/assets/img/logo.svg" alt="" />
+            <form action='index.html'>
+                <div className='signin-details'>
+                    <h3>Sign In</h3>
+                    <p style={{color : '#9DA4B0'}}>Please enter below details to access the dashboard</p>
+                </div>
+                <div className='email-pass'>     
+                    <label htmlFor="">Email Address</label>
+                    <div className='inputs'>
+                        <i class="fa-regular fa-envelope"></i>
+                        <input type="text" />
+                    </div>
+                    <label htmlFor="">Password</label>
+                    <div className='inputs'>
+                        <i class="fa-regular fa-envelope"></i>
+                        <input type="text" />
+                    </div>
+                    
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </form>
+        </div>
+    </>
+  )
+}
+
+export default Login
