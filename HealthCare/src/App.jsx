89e529edf1f89c59eb79fc390ccfaa6345f9919a
vinaybabare <babare.vinayak@1997gmail.com>
2025-06-30 15:10:27 +0530

    hello i am vinayak

--- HealthCare/src/App.jsx
+++ HealthCare/src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './Components/Login'
-import LogoHead from './Components/LogoHead'
 import LoginPage from "./register/LoginPage"
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
-    {/* <LogoHead/>   */}
-    {/* <Login/> */}
-    <LoginPage/>
+      <LoginPage />
     </>
   )
 }
